92f574c165552e51e87bbf9a7467d3650d161272
Alexander Colloredo-Mansfeld <acollore@ucsc.edu>
2021-03-12 20:04:21 -0800

    Firebase-less comment section. Todo: connect firebase

---
+++ Flutter/slug_app/lib/screens/comments.dart
@@ -0,0 +1,46 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+class CommentsPage extends StatefulWidget {
+  @override
+  createState() => new CommentsPageState();
+}
+
+class CommentsPageState extends State<CommentsPage> {
+  List<String> _comments = [];
+
+  void _addComment(String val) {
+    setState(() {
+      _comments.add(val);
+    });
+  }
+
+  Widget _buildCommentList() {
+    return ListView.builder(itemBuilder: (context, index) {
+      if (index < _comments.length) {
+        return _buildCommentItem(_comments[index]);
+      }
+    });
+  }
+
+  Widget _buildCommentItem(String comment) {
+    return ListTile(title: Text(comment));
+  }
+
+  @override
+  Widget build(BuildContext contect) {
+    return Scaffold(
+        appBar: new AppBar(title: Text("Comments")),
+        body: Column(children: <Widget>[
+          Expanded(child: _buildCommentList()),
+          TextField(
+            onSubmitted: (String submittedStr) {
+              _addComment(submittedStr);
+            },
+            decoration:
+                InputDecoration(contentPadding: const EdgeInsets.all(20.0)),
+          )
+        ]));
+  }
+}
